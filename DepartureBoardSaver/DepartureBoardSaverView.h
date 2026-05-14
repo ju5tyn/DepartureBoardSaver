7153bd39ec8f5c59c056c07fa7a8b7dce8854576
Justyn <justynhenman@me.com>
2026-05-14 22:54:56 +0100

    Initial Commit

---
+++ DepartureBoardSaver/DepartureBoardSaverView.h
@@ -0,0 +1,12 @@
+//
+//  DepartureBoardSaverView.h
+//  DepartureBoardSaver
+//
+//  Created by Justyn Henman on 14/05/2026.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface DepartureBoardSaverView : ScreenSaverView
+
+@end
---
+++ DepartureBoardSaver/DepartureBoardSaverView.m
@@ -0,0 +1,51 @@
+//
+//  DepartureBoardSaverView.m
+//  DepartureBoardSaver
+//
+//  Created by Justyn Henman on 14/05/2026.
+//
+
+#import "DepartureBoardSaverView.h"
+
+@implementation DepartureBoardSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
